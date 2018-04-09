15b644931e171e203bf5d3f3c7b091a2017ed5dd
saumya-singh <saumya.singh0993@gmail.com>
2018-04-09 08:54:28 +0530

    to populate postgres database from csv file

---
+++ populate.py
@@ -0,0 +1,87 @@
+import re
+import csv
+import psycopg2
+
+
+conn = psycopg2.connect(database="songspedia", user="saumya",
+                        password="password@123", host="127.0.0.1")
+conn.autocommit = True
+print("Opened database successfully")
+
+cur = conn.cursor()
+cur.execute('''CREATE TABLE IF NOT EXISTS ARTISTS
+      (POPULARITY    TEXT       NOT NULL,
+      ID CHAR(50) PRIMARY KEY   NOT NULL,
+      NAME           TEXT       NOT NULL,
+      LATITUDE       TEXT       NOT NULL,
+      LONGITUDE      TEXT       NOT NULL);''')
+print("Table1 created successfully")
+
+
+cur.execute('''CREATE TABLE IF NOT EXISTS SONGS
+     (ID CHAR(50) PRIMARY KEY  NOT NULL,
+     TEMPO        TEXT         NOT NULL,
+     TERMS        TEXT     NOT NULL,
+     TITLE        TEXT     NOT NULL,
+     YEAR         TEXT);''')
+print("Table2 created successfully")
+
+
+cur.execute('''CREATE TABLE IF NOT EXISTS RELEASES
+      (ID CHAR(50) PRIMARY KEY     NOT NULL,
+      ARTIST_ID CHAR(50)  REFERENCES ARTISTS(ID),
+      SONG_ID   CHAR(50)  REFERENCES SONGS(ID),
+      NAME           TEXT          NOT NULL);''')
+print("Table3 created successfully")
+
+
+def clean_code(row):
+    regex = re.compile('[^-a-zA-Z0-9_.!/() ]')
+    clean_data = []
+    for each_detail in row:
+        clean_data.append(regex.sub('', each_detail))
+    return clean_data
+
+
+spamReader = csv.reader(open('music.csv'), delimiter='\n')
+c = next(spamReader)
+for row in spamReader:
+    raw_details = row[0].split(",")
+    details = clean_code(raw_details)
+    print("===", details)
+
+    query1 = "INSERT INTO ARTISTS (ID,NAME,POPULARITY,LATITUDE,LONGITUDE) \
+              VALUES (\'{0}\', \'{1}\', \'{2}\', \'{3}\', \'{4}\')".format(
+              details[1], details[2], details[0], details[3], details[4])
+    query3 = "INSERT INTO SONGS (ID,TITLE,TEMPO,TERMS,YEAR) \
+              VALUES (\'{0}\', \'{1}\', \'{2}\', \'{3}\', \'{4}\')".format(
+              details[7], details[10], details[8], details[9], details[11])
+
+    try:
+        cur.execute(query1)
+    except Exception as e:
+        print(e)
+
+    try:
+        cur.execute(query3)
+    except Exception as e:
+        print(e)
+
+
+spamReader = csv.reader(open('music.csv'), delimiter='\n')
+c = next(spamReader)
+for row in spamReader:
+    raw_details = row[0].split(",")
+    details = clean_code(raw_details)
+    query2 = "INSERT INTO RELEASES (ID,NAME,ARTIST_ID,SONG_ID) \
+              VALUES (\'{0}\', \'{1}\', \'{2}\', \'{3}\')".format(
+              details[5], details[6], details[1], details[7])
+
+    try:
+        cur.execute(query2)
+    except Exception as e:
+        print(e)
+
+
+conn.commit()
+conn.close()
